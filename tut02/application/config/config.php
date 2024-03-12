cb36550e51410edac3e61fb17a0ae9668b2f9c00
Purna Peramune <purnaperamune@gmail.com>
2024-03-12 12:11:27 +0530

    minor changes

--- tut02/application/config/config.php
+++ tut02/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/AdvanceServerSideTutorials/Tut02/';
+$config['base_url'] = 'http://localhost:8080/AdvanceServerSideTutorials/tut02/';
 
 /*
 |--------------------------------------------------------------------------
