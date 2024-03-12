0e1c104dc6283b9c68f891ed8337a71a4677d7b4
Purna Peramune <purnaperamune@gmail.com>
2024-03-12 11:56:33 +0530

    Made a view and displaying it through a controller

---
+++ tut02/application/views/student_details.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>
+        Name: Purna Peramune <br>
+        School: DS Senanayake College Col. 07 <br>
+        Degree: BEng (Hons) in Software Engineering, University of Westminster, UK.
+    </h3>
+</body>
+</html>
